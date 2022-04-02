488556c2870bdf6c764d13508010415b4ad1c7bd
zby-zy <zynbbayrak911@gmail.com>
2022-04-02 18:52:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,30 +14,30 @@ class Calc(object):
     
     def divide(self):
         return self.value1/self.value2
-   
+    
+print("Select operation.\n1. Add\n2. Subtract\n3. Multiply\n4. Divide")
+
 v1 = int(input("Enter your 1st number: "))
 v2 = int(input("Enter your 2nd number: "))
 c1 = Calc(v1, v2)    
-
-print("Choose add(1), multiply(2), sub(3), divide(4)!")
 
 selection = input("Select 1, 2 , 3 or 4: ")
 
 if selection == "1":
     add_result = c1.add()
-    print("Add: {}".format(add_result))
+    print("Addition: {}".format(add_result))
 
 elif selection == "2":
     multiply_result = c1.multiply()
-    print("Multiply: {}".format(multiply_result))
+    print("Multiplication: {}".format(multiply_result))
 
 elif selection == "3":
     sub_result = c1.sub()   
-    print("Sub: {}".format(sub_result))    
+    print("Subtraction: {}".format(sub_result))    
     
 elif selection == "4":
     divide_result = c1.divide()
-    print("Divide: {}".format(divide_result))
+    print("Division: {}".format(divide_result))
     
 else:
     print("Error!")        
